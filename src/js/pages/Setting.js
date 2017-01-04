a7e34afd0a3a958845d8af90753031a13cb30ef2
shisaq <shisaq@gmail.com>
2017-01-04 10:36:41 -0500

    remove: remove unnecessary time picker of setting

--- src/js/pages/Setting.js
+++ src/js/pages/Setting.js
@@ -3,13 +3,12 @@ import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import DatePicker from 'material-ui/DatePicker';
-import TimePicker from 'material-ui/TimePicker';
 import FlatButton from 'material-ui/FlatButton';
 
 export default class Setting extends React.Component {
     render() {
         return (
-            <Paper>
+            <Paper zDepth={4}>
                 倒计时名称：<TextField name="projectName" />
                 <RadioButtonGroup name="timeType" defaultSelected="deadline">
                     <RadioButton
@@ -22,7 +21,6 @@ export default class Setting extends React.Component {
                     />
                 </RadioButtonGroup>
                 <DatePicker hintText="点击选择截止日期" />
-                <TimePicker hintText="点击选择截止时间" />
                 <TextField name="duration" />天
                 <FlatButton label="现在开始倒计时！" />
             </Paper>
